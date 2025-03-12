8593a5e87f453a06d5cd766541334e51df8499ad
LuisHenriqueXavier <luishenriquexavier@outloo.com>
2025-03-12 04:16:51 -0300

    Feat: Conexão com a API - Configurando JSON Server

--- src/components/Header/NewTransactionModal/styled.ts
+++ src/components/Header/NewTransactionModal/styled.ts
@@ -1,5 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import styled from "styled-components";
+import * as RadioGroup  from "@radix-ui/react-radio-group";
 
 export const Overlay = styled(Dialog.Overlay)`
     position: fixed; 
@@ -69,7 +70,7 @@ export const CloseButton = styled(Dialog.Close)`
     color: ${props => props.theme["gray-500"]};
 `;
 
-export const TansactionType = styled.div`
+export const TansactionType = styled(RadioGroup.Root)`
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     gap: 1rem;
@@ -81,7 +82,7 @@ interface TransactionTypeButtonProps {
     variant: "income" | "outcome";
 }
 
-export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+export const TransactionTypeButton = styled(RadioGroup.Item)<TransactionTypeButtonProps>`
     background: ${props => props.theme["gray-700"]};
     padding: 1rem;
     display: flex;
@@ -95,4 +96,22 @@ export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
 
 svg {
     color: ${props => props.theme[props.variant === "income" ? "green-300" : "red-300"]};
+}
+
+&[data-state="unchecked"]:hover {
+        background: ${props => props.theme["gray-600"]};
+        transition: background-color 0.2;
+    }
+
+&:hover {
+    background: ${props => props.theme["gray-800"]};
+    transition: background-color 0.2s;
+}&[data-state="checked"] {
+    color: ${props => props.theme.white};
+    background: ${props => props.theme[props.variant === "income" ? "green-500" : "red-500"]};
+
+    svg {
+        color: ${props => props.theme.white};
+    }
+}
 `;

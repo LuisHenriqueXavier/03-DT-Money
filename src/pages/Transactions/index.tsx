26d5bbf83fac9ff25b1e48c3fcf24a569eb2eb59
Luís Henrique Xavier <luishenriquexavier@outlook.com>
2025-05-02 00:36:00 -0300

    Feat: Performance - Aplicando o useCallback

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,4 +1,3 @@
-import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm/index.tsx";
@@ -9,9 +8,12 @@ import {
 } from "./styles";
 import { TransactionsContext } from "../../contexts/TransactionsContext.tsx";
 import { dateFormatter, priceFormatter } from "../../utils/formater.ts";
+import { useContextSelector } from "use-context-selector";
 
 export function Transactions() {
-  const { transactions } = useContext(TransactionsContext);
+  const transactions = useContextSelector(TransactionsContext, (context) => {
+    return context.transactions;
+  });
 
   return (
     <div>
